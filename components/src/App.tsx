dd7ecbfab83808643e70fbdf354dec0adcbea4ef
Voloshin Sergei <4voloshin@gmail.com>
2022-03-24 16:39:02 +0300

    feat: added styles for Header

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Header } from './components/Header/Header';
-import './App.css';
+import { Header } from './components/Header';
+import './App.module.scss';
 
 function App() {
   return <Header />;
--- components/src/components/Header/Header.tsx
+++ components/src/components/Header/Header.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import logo from '../../img/logo.png';
+import bg from '../../img/header_bg.png';
 
-import style from './Header.module.css';
+import style from './Header.module.scss';
 
 export class Header extends React.Component {
   render() {
     return (
       <header className={style.header}>
-        <img className={style.logo} src={logo} width="200" />
-        <nav className={style.nav}>
-          <ul className={style.nav_list}>
-            <li className={style.nav_item}>Home</li>
-            <li className={style.nav_item}>About us</li>
-          </ul>
-        </nav>
+        <div className={style.header__container}>
+          <div className={style.header__wrapper}>
+            <a href="/">
+              <img className={style.header__logo} src={logo} />
+            </a>
+            <nav className={style.nav}>
+              <ul className={style.nav__list}>
+                <li className={style.nav__item}>Home</li>
+                <li className={style.nav__item}>About us</li>
+              </ul>
+            </nav>
+          </div>
+          <img className={style.header__bg} src={bg} />
+        </div>
       </header>
     );
   }

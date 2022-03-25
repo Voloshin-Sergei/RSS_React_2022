4de0571bc2a3e863f7ef751cced478fb8d2f603c
Voloshin_Sergey <4voloshin@gmail.com>
2022-03-25 08:09:35 +0300

    feat: added footer component

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { Main } from './components/Main';
+import { Footer } from './components/Footer';
 
 import './App.module.scss';
 
@@ -10,6 +11,7 @@ export class App extends React.Component {
       <>
         <Header />
         <Main />
+        <Footer />
       </>
     );
   }

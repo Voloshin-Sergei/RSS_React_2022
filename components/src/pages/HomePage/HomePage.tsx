f67326fd370fd3b90d90db1fd5c43570eb52138a
Voloshin Sergei <4voloshin@gmail.com>
2022-04-11 16:38:40 +0300

    refactor: fetch response

--- components/src/pages/HomePage/HomePage.tsx
+++ components/src/pages/HomePage/HomePage.tsx
@@ -7,6 +7,7 @@ import { Person } from '../../types';
 interface HomePageState {
   persons: Person[];
   isLoader: boolean;
+  error: null;
 }
 
 interface HomePageProps {
@@ -20,20 +21,25 @@ export class HomePage extends React.Component<HomePageProps, HomePageState> {
     this.state = {
       persons: [],
       isLoader: false,
+      error: null,
     };
   }
 
   url = 'https://rickandmortyapi.com/api/';
 
   getCharacters = (name: string) => {
-    fetch(`${this.url}character?name=${name}`)
-      .then((response) => response.json())
-      .then((response) => {
-        this.setState({ persons: response.results });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    this.setState({ persons: [] });
+    fetch(`${this.url}character?name=${name}`).then(async (response) => {
+      this.setState({ isLoader: true });
+      const data = await response.json();
+
+      if (response.ok) {
+        this.setState({ persons: data.results });
+      }
+
+      this.setState({ error: data.error });
+      this.setState({ isLoader: false });
+    });
   };
 
   render() {
@@ -44,6 +50,8 @@ export class HomePage extends React.Component<HomePageProps, HomePageState> {
         {this.state.isLoader && <Loader />}
 
         {this.state.persons.length && <ItemList persons={this.state.persons} />}
+
+        {this.state.error && <h1>{this.state.error}</h1>}
       </>
     );
   }
